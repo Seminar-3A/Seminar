5855f961b6cf46e83db743033490d83e1df72bf7
karlbouabboud <karlbouabboud@gmail.com>
2016-11-19 14:50:01 +0100

    added creation of classes

--- data_extraction.py
+++ data_extraction.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 main_feat = ['High', 'Low', 'Close']
 pd.set_option('chained_assignment', None)
-
+default_limit_classes = ["-inf", -5, -2, 0, 2, 5, "+inf"]
 
 def get_raw_data(stock_name, start, stop, features=main_feat, target="variation"):
 
@@ -46,7 +46,7 @@ def get_raw_data(stock_name, start, stop, features=main_feat, target="variation"
     return raw_data
 
 
-def frmt_raw_data(stock_name, start, stop, raw_data=DataFrame(), features=main_feat, target="variation"):
+def frmt_raw_data(stock_name, start, stop, raw_data=DataFrame(), features=main_feat, target="variation", limit_classes=default_limit_classes):
     """
 
     :param stock_name: string
@@ -55,6 +55,7 @@ def frmt_raw_data(stock_name, start, stop, raw_data=DataFrame(), features=main_f
     :param raw_data: DataFrame
     :param features: array of strings
     :param target: string "variation" default or "log"
+    :param limit_classes: array containing limits of the daily close variation classes
     :return: format the raw history data and add to each observation the actual prediction of close return
     """
     if (raw_data.empty):
@@ -66,10 +67,31 @@ def frmt_raw_data(stock_name, start, stop, raw_data=DataFrame(), features=main_f
     elif raw_data.columns[-1] == "Variation_Close":
         raw_data.Variation_Close = raw_data.Variation_Close.shift(-1)
         raw_data.columns = [main_feat + ['Tmrw_variation']]
+        raw_data["variation_classes"] = divide_in_classes(raw_data["Tmrw_variation"], limit_classes)
     raw_data = raw_data.dropna()
     raw_data['Ticker'] = stock_name
 
     return raw_data
+
+
+def divide_in_classes(variation_vector, limit_classes=default_limit_classes):
+    """
+    :param variation_vector: vector of float containing daily variation ratio
+    :param limit_classes: vector containing the limits of the different classes of daily close variation
+    :return: vector of strings
+    """
+    classes = ["Between "+str(limit_classes[i])+" and "+str(limit_classes[i+1]) for i in range(0, len(limit_classes)-1)]
+    classes_col = np.empty(len(variation_vector),dtype=object)
+    for i in range(0, len(variation_vector)):
+        if variation_vector[i] <= limit_classes[1]:
+            classes_col[i] = classes[0]
+        elif variation_vector[i] > limit_classes[-2]:
+            classes_col[i] = classes[-1]
+        else:
+            for y in range(1, len(limit_classes)-2):
+                if limit_classes[y] < variation_vector[i] <= limit_classes[y+1]:
+                    classes_col[i] = classes[y]
+    return classes_col
 
 
 def get_multiple_inputs(stock_list, start, stop):
