76dedea7301ab867c24418c6e2a3b2c6067d29bc
BERRADA Karim <berrada.ahmed.karim@gmail.com>
2016-12-07 18:55:55 +0100

    some changes, debugging, and random forest fitting

--- constantes.py
+++ constantes.py
@@ -1,5 +1,6 @@
-default_limit_classes = ["-inf", -5, -2, 0, 2, 5, "+inf"]
+default_limit_classes = [-5, -2, 0, 2, 5]
 main_feat = ['High', 'Low', 'Close']
 min_range = -0.02
 max_range = 0.02
 step_range = 0.005
+yclass_label = 'Tmrw_Class'
--- data_extraction.py
+++ data_extraction.py
@@ -8,7 +8,7 @@ from pandas.io.data import DataReader
 import pandas as pd
 import numpy as np
 
-from constantes import main_feat, default_limit_classes
+from constantes import main_feat, default_limit_classes, yclass_label
 from utils_rf import subdivide_data
 from random_forest import fitting_forest
 
@@ -47,7 +47,7 @@ def get_raw_data(stock_name, start, stop, features=main_feat):
     return raw_data
 
 
-def get_data_with_past(stock_name, start, stop, features=main_feat, nb_past_days = 0):
+def get_data_with_past(stock_name, start, stop, features=main_feat, nb_past_days=0):
     """
     :param stock_name: string
     :param start: string
@@ -92,21 +92,21 @@ def get_ret_class(raw_data, ret_ranges):
     The class zero correspond to the lowest return level
 
     """
-    raw_data['Tmrw_Class'] = np.nan
+    raw_data[yclass_label] = np.nan
     # We label diffently the following extreme return values :
     # The returns below the lowest return range and the returns above the highest return range
-    raw_data.loc[(raw_data['Tmrw_return'] < ret_ranges[0]),'Tmrw_Class'] = 0
-    raw_data.loc[(raw_data['Tmrw_return'] >= ret_ranges[-1]), 'Tmrw_Class'] = len(ret_ranges)
+    raw_data.loc[(raw_data['Tmrw_return'] < ret_ranges[0]), yclass_label] = 0
+    raw_data.loc[(raw_data['Tmrw_return'] >= ret_ranges[-1]), yclass_label] = len(ret_ranges)
 
     for i in range(len(ret_ranges)-1):
         ret_level_min = ret_ranges[i]
         ret_level_max = ret_ranges[i+1]
-        raw_data.loc[(raw_data['Tmrw_return'] >= ret_level_min)&(raw_data['Tmrw_return'] < ret_level_max), 'Tmrw_Class'] = i+1
+        raw_data.loc[(raw_data['Tmrw_return'] >= ret_level_min)&(raw_data['Tmrw_return'] < ret_level_max), yclass_label] = i+1
 
     return raw_data
 
 
-def frmt_raw_data(stock_name, start, stop, ret_ranges=default_limit_classes, raw_data=DataFrame(),features=main_feat):
+def frmt_raw_data(stock_name, raw_data, ret_ranges=default_limit_classes):
 
     """
     :param stock_name: string
@@ -117,8 +117,6 @@ def frmt_raw_data(stock_name, start, stop, ret_ranges=default_limit_classes, raw
     :return: Format the raw history data and add to each observation the actual prediction of close return
 
     """
-    if raw_data.empty:
-        raw_data = get_raw_data(stock_name, start, stop,features)
 
     raw_data['Ticker'] = stock_name
     frmt_data = get_ret_class(raw_data, ret_ranges)
@@ -126,40 +124,25 @@ def frmt_raw_data(stock_name, start, stop, ret_ranges=default_limit_classes, raw
     return frmt_data
 
 
-def get_multiple_inputs(stock_list, start, stop):
-    """
-
-    :param stock_list: list of stocks
-    :param start:
-    :param stop:
-    :param features:
-    :return:
-    """
-
-    frames = [frmt_raw_data(stock_list[i], start, stop,
-                            ret_ranges=default_limit_classes,
-                            raw_data=DataFrame(),
-                            features=main_feat) for i in range(len(stock_list))]
-
-    df_input = pd.concat(frames)
-
-    return df_input
-
 if __name__ == "__main__":
     stock_name = sys.argv[1]
     start = sys.argv[2]
     stop = sys.argv[3]
-    raw_data = get_raw_data(stock_name, start, stop, features=main_feat)
-    frmt_data = frmt_raw_data(stock_name, start, stop,
-                              ret_ranges=default_limit_classes,
-                              raw_data=raw_data,
-                              features=main_feat)
+    nb_past_days = sys.argv[4]
+    raw_data = get_data_with_past(stock_name, start, stop, features=main_feat, nb_past_days=nb_past_days)
+    # Getting the features names
+    X_features = raw_data.columns.tolist()
+    y_index = X_features.index('Tmrw_return')
+    del X_features[y_index]
+    # Formatting Data by labeling the returns for the classification
+    frmt_data = frmt_raw_data(stock_name, raw_data, ret_ranges=default_limit_classes)
+
     print("Done downloading and formatting the input data, saving it...")
     frmt_data.to_csv("Input_data.csv")
 
     # Fitting the Random Forest
-    input_X = frmt_data[main_feat]
-    input_Y = frmt_data['variation_classes']
+    input_X = frmt_data[X_features]
+    input_Y = frmt_data[yclass_label]
     data_subdivided = subdivide_data(input_X, input_Y, test_size=0.3)
 
     fit_forest, score, prediction = fitting_forest(data_subdivided, n_estimators=100)
